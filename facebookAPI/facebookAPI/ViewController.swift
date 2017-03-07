fd63ca0eedbe847de84d53ce2d16a0de00788ef1
0wnrepo <0wnrepo@localhost.local>
2017-03-07 14:52:53 +0200

    Initial commit for facebookAPI project

---
+++ facebookAPI/facebookAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  facebookAPI
+//
+//  Created by Good on 07/03/2017.
+//  Copyright © 2017 Good. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
