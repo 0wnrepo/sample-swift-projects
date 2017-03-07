46afdb18e74bdb10143ae1fa8ecadaea01db059c
0wnrepo <0wnrepo@localhost.local>
2017-03-07 14:21:46 +0200

    Added input textview to check if string input is a valid prime number

--- primes/ViewController.swift
+++ primes/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var isPrimeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func isPrimeButtonAction(_ sender: Any) {
+        let numberString = textView.text
 
+        checkIfPrimeFromWithString(input: numberString!) {
+            (result: String) in
+            print("got back: \(result)",
+            statusLabel.text = result)
+        }
+        
+    }
+    
+    func checkIfPrimeFromWithString(input: String, completion: (_ result: String) -> Void) {
+        let n = Double(input)
+        
+        if Int(n!) % 2 == 0 {
+            completion("Not prime! divisor: 2")
+            return
+        }
+        
+        let first = 3
+        let last = Int(floor(sqrt(n!)))
+        let interval = 2
+        
+        for i in stride(from: first, through: last, by: interval) {
+            if (Int(n!) % i == 0) {
+                completion("Not prime! divisor: \(i)")
+                return
+            }
+        }
+        
+        completion("prime!")
+    }
+    
+    
 
 }
 
